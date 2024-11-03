4e7a49c5031124b695842da025b57b953bfde8ac
ezegy <ezegy@DESKTOP-9POM430>
2024-11-03 20:05:23 +0100

    Add project files.

---
+++ SzamTesztOOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SzamTesztOOP;
+
+namespace SzamTesztOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SzamTeszt teszt1 = new SzamTeszt();
+            teszt1.SetSzam1(7);
+            teszt1.SetSzam2(7);
+            teszt1.SetEredmeny();
+            Console.WriteLine(teszt1.GetEredmeny());
+
+            SzamTeszt teszt2 = new SzamTeszt();
+            teszt2.SetSzam1(3);
+            teszt2.SetSzam2(8);
+            teszt2.SetEredmeny();
+            Console.WriteLine(teszt2.GetEredmeny());
+        }
+    }
+}
---
+++ SzamTesztOOP/SzamTeszt.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzamTesztOOP
+{
+    internal class SzamTeszt
+    {
+        private int szam1;
+        private int szam2;
+        private int eredmeny;
+
+        
+        public SzamTeszt() { }
+
+        
+        public SzamTeszt(int szam1, int szam2)
+        {
+            this.szam1 = szam1;
+            this.szam2 = szam2;
+        }
+
+        
+        public void SetSzam1(int szam1)
+        {
+            this.szam1 = szam1;
+        }
+
+        
+        public void SetSzam2(int szam2)
+        {
+            this.szam2 = szam2;
+        }
+
+        
+        public void SetEredmeny()
+        {
+            if (szam1 == szam2)
+            {
+                eredmeny = 0; 
+            }
+            else if (szam1 > szam2)
+            {
+                eredmeny = 1; 
+            }
+            else
+            {
+                eredmeny = 2; 
+            }
+        }
+
+        
+        public int GetEredmeny()
+        {
+            return eredmeny;
+        }
+    }
+}
